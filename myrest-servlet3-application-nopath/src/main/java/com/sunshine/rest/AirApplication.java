1567020ebe326f28fee617df533b0dc13d5d8dfc
zhangm <sunshine.zhangm@gmail.com>
2017-01-11 23:38:39 +0800

    myrest-servlet3-application-nopath

---
+++ myrest-servlet3-application-nopath/src/main/java/com/sunshine/rest/AirApplication.java
@@ -0,0 +1,17 @@
+package com.sunshine.rest;
+
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by zhangm on 2017/1/11.
+ */
+public class AirApplication extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> classes = new HashSet<>();
+        classes.add(MyResource.class);
+        return classes;
+    }
+}
