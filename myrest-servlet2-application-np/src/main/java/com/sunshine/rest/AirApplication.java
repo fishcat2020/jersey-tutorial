57283e24a489676664ce530fbee27937e9ce2c0f
zhangm <sunshine.zhangm@gmail.com>
2017-01-12 11:07:51 +0800

    myrest-servlet2-application-np

---
+++ myrest-servlet2-application-np/src/main/java/com/sunshine/rest/AirApplication.java
@@ -0,0 +1,14 @@
+package com.sunshine.rest;
+
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+public class AirApplication extends Application {
+	@Override
+	public Set<Class<?>> getClasses() {
+		final Set<Class<?>> classes = new HashSet<Class<?>>();
+		classes.add(MyResource.class);
+		return classes;
+	}
+}
